ce9a0da93140f561df252e02baed581b0ed87f69
jobinben <1021137079@qq.com>
2022-03-05 15:50:04 +0800

    practice 链表相交

--- 代码随想录/链表/06.js
+++ 代码随想录/链表/06.js
@@ -26,3 +26,35 @@ const detectCycle = (head) => {
     return null
 
 };
+
+
+// 判断链表是否相交
+
+var detectCycle = function(head) {
+    let slow = head;
+    let fast = head;
+
+    // 1. 快指针不能等于空
+    while(fast !== null && fast.next !== null) {
+        // 2. 快慢指针走
+        slow = slow.next;
+        fast = fast.next.next;
+
+        // 3. 当slow 和 fast 相等时，此时不一定是环的起点
+        // 有环必定相遇
+        if(slow === fast) {
+            // 4. 下面是找出环的起点pos
+            let index1 = fast;
+            let index2 = head;
+
+            while(index1 !== index2) {
+                index1 = index1.next;
+                index2 = index2.next;
+            }
+
+            return index2;
+        }
+    }
+
+    return null;
+};
